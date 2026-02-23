540eae68c2e5b4ba8d8bb1d254f7d583db5508fe
Kaio Grativol Baldo <78832356+KaioGBaldo@users.noreply.github.com>
2026-02-23 10:50:15 -0300

    Add ProductViewSet and update OrderViewSet

--- products/views.py
+++ products/views.py
@@ -1,8 +1,12 @@
 from rest_framework import viewsets
-from .models import Order
-from .serializers import OrderSerializer
+from products.models import Product, Order, Category
+from products.serializers import ProductSerializer, OrderSerializer, CategorySerializer
 
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+# ADICIONE ESTA CLASSE:
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
-    # Se a paginação global não estiver no settings.py, ela deve ser declarada aqui
