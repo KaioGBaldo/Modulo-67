bf8019368405e765f51c5f976f3f281b32443ce3
Kaio Grativol Baldo <78832356+KaioGBaldo@users.noreply.github.com>
2026-02-23 10:46:56 -0300

    Update views.py

--- products/views.py
+++ products/views.py
@@ -1,7 +1,8 @@
-from rest_framework.viewsets import ModelViewSet
-from .models import Produto
-from .serializers import ProdutoSerializer
+from rest_framework import viewsets
+from .models import Order
+from .serializers import OrderSerializer
 
-class ProdutoViewSet(ModelViewSet):
-    queryset = Produto.objects.all()
-    serializer_class = ProdutoSerializer
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+    # Se a paginação global não estiver no settings.py, ela deve ser declarada aqui
