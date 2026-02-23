2bca37a596f9e3d347a437b83ca8af8751fd549a
Kaio Grativol Baldo <78832356+KaioGBaldo@users.noreply.github.com>
2026-02-23 10:49:56 -0300

    Update urls.py

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -1,12 +1,15 @@
+from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ProductViewSet, OrderViewSet # Verifique se ambos estão importados
+from products import views # Importa as views do seu app 'products'
 
 router = DefaultRouter()
-router.register(r'product', ProductViewSet, basename='product')
-# A correção solicitada pelo professor está nesta linha:
-router.register(r'order', OrderViewSet, basename='order')
+router.register(r'product', views.ProductViewSet, basename='product')
+router.register(r'category', views.CategoryViewSet, basename='category')
+# ADICIONE ESTA LINHA:
+router.register(r'order', views.OrderViewSet, basename='order')
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('admin/', admin.site.urls),
+    path('api/', include(router.urls)), # Suas rotas ficarão em api/order/
 ]
