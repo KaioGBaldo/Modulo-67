6da4c360a4f97c83b356afe7bea18f26be94e754
Kaio Grativol Baldo <78832356+KaioGBaldo@users.noreply.github.com>
2026-03-28 23:35:16 -0300

    Update urls.py

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -1,14 +1,13 @@
-from django.contrib import admin
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from products import views
-
-router = DefaultRouter()
-router.register(r'product', views.ProductViewSet, basename='product')
-router.register(r'category', views.CategoryViewSet, basename='category')
-router.register(r'order', views.OrderViewSet, basename='order') # ESTA LINHA É A QUE FALTA!
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
+    path('api/', include('products.urls')),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
