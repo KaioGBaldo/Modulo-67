a42b5c9e237f935689fb076fd5053f15de07232d
Kaio Grativol Baldo <78832356+KaioGBaldo@users.noreply.github.com>
2026-04-09 22:50:18 -0300

    Update urls.py

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ProductViewSet
+from .views import ProductViewSet # Certifique-se de importar os outros se criar as views
 
-# O Router do DRF cria automaticamente as rotas de GET, POST, PUT e DELETE
 router = DefaultRouter()
 router.register(r'product', ProductViewSet, basename='product')
+# Se você criar as ViewSets para os outros, registre-os aqui:
+# router.register(r'category', CategoryViewSet, basename='category')
 
 urlpatterns = [
     path('', include(router.urls)),
