5f91f94216d32c9333e52b2ed02581032ba44899
Kaio Grativol Baldo <78832356+KaioGBaldo@users.noreply.github.com>
2026-04-15 21:01:47 -0300

    Update views.py

--- products/views.py
+++ products/views.py
@@ -1,7 +1,12 @@
 from rest_framework import viewsets
-from .models import Product
-from .serializers import ProductSerializer
+from .models import Product, Order  # Adicione Order aqui
+from .serializers import ProductSerializer, OrderSerializer # Adicione OrderSerializer
 
 class ProductViewSet(viewsets.ModelViewSet):
-    queryset = Product.objects.all().order_by('id') # Importante ordenar para paginar
+    queryset = Product.objects.all().order_by('id')
     serializer_class = ProductSerializer
+
+# ADICIONE ESTA CLASSE:
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all().order_by('-created_at')
+    serializer_class = OrderSerializer
